078bccb1c02463eb1a0bd9f375f5fbe1537e7ec5
Lazarus <git@devbeejohn.de>
2025-11-28 11:31:18 +0100

    Remove locale instruction

--- src/Service/EvolutionDataService.mm
+++ src/Service/EvolutionDataService.mm
@@ -7,7 +7,6 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 #include "EvolutionDataService.h"
-#include <clocale>
 #include <peel/GLib/functions.h>
 
 using namespace peel;
@@ -16,7 +15,6 @@ namespace C2P {
 RefPtr<Gio::ListStore>
 EvolutionDataService::getAddressbookSources()
 {
-    setlocale(LC_ALL, "");
     UniquePtr<GLib::Error> error;
 
     auto registry = EDataServer::SourceRegistry::create_sync(/* cancellable */ nullptr, &error);
